dba59edbadd7c485a5bf3830184b6f242f96fe85
kpethtel <kris.pethtel@gmail.com>
2020-08-23 11:56:47 -0400

    add orders index and show

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,14 +1,31 @@
+require 'pry'
 class OrdersController < ApplicationController
-  before_action :find_order!, only: :show
 
   def index
+    orders
   end
 
   def show
+    order
+    product_items
   end
 
   private
-  def find_order!
-    @order = Order.find_by!(number: params[:number])
+  def order
+    @order ||=
+      Order.includes(:user, :payments, order_items: [:source]).find_by!(number: permitted_params[:number])
+  end
+
+  def orders
+    @orders ||= Order.includes(:user).all
+  end
+
+  def product_items
+    @product_items ||= order.order_items.select {|item| item.source_type == "Product" }
+  end
+
+  PERMITTED_PARAMS = %i[number]
+  def permitted_params
+    params.permit(PERMITTED_PARAMS)
   end
 end
